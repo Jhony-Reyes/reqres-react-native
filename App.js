bf34c844d35cf0d273a7c62310ac329eff41452b
Jhony Reyes <jhonysr19@gmail.com>
2020-09-16 19:41:55 -0500

    Add react-navigation config

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
+/*
+ * @Author: Jhony Reyes
+ * @Date: 2020-09-16 18:33:46
+ * @Last Modified by: Jhony Reyes
+ * @Last Modified time: 2020-09-16 19:40:52
+ */
+
+import 'react-native-gesture-handler';
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
